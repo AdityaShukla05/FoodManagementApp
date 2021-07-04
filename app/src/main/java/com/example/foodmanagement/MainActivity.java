a245e0a7fff5bcd93bbe5caf9136c643f57f1800
Aditya Shukla <adi05shukla2001@gmail.com>
2021-07-04 22:06:31 +0530

    added toolbar and minor changes in btn

--- app/src/main/java/com/example/foodmanagement/MainActivity.java
+++ app/src/main/java/com/example/foodmanagement/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.foodmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import androidx.appcompat.widget.Toolbar;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar =  findViewById(R.id.toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setActionBar(mToolbar);
+        }
     }
 }
